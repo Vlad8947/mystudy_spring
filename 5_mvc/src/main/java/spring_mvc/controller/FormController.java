3b89fdb66feb7d6e98d9f9bedb493831b29765a8
Vlad8947 <31621413+Vlad8947@users.noreply.github.com>
2019-07-01 01:03:14 +0300

    Update FormController.java

--- 5_mvc/src/main/java/spring_mvc/controller/FormController.java
+++ 5_mvc/src/main/java/spring_mvc/controller/FormController.java
@@ -1,36 +1,38 @@
 package spring_mvc.controller;
 
+import com.sun.tracing.dtrace.ModuleAttributes;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import spring_mvc.model.HumanModel;
 
-//todo form dosn't work
-
 @Controller
-//@RequestMapping("/form")
+@RequestMapping("/form")
 public class FormController {
 
     public FormController() {
     }
 
-//    @GetMapping(params = "form")
-//    public String getForm(Model model) {
-//        HumanModel human = new HumanModel();
-//        model.addAttribute("human", human);
-//        return "form";
-//    }
+    @GetMapping
+    public String getForm(Model model) {
+        HumanModel human = new HumanModel();
+        model.addAttribute("human", human);
+        return "form";
+    }
 
-//    @PostMapping(params = "form")
-//    public String form(BindingResult bindingResult, HumanModel human) {
-//        if (!bindingResult.hasErrors()) {
-//            System.out.println("name" + human.toString());
-//        }
-//        return "hello";
-//    }
+    @PostMapping
+    public String form(@ModelAttribute("human") HumanModel human,
+                       BindingResult bindingResult,
+                       Model model) {
+        if (!bindingResult.hasErrors()) {
+            model.addAttribute("name", human.toString());
+        }
+        return "redirect:/hello-name2";
+    }
 }
