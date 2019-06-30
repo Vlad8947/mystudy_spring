68a1d3e2d3ada0dfe6134b1345ab2b3316cc1acd
Vlad8947 <31621413+Vlad8947@users.noreply.github.com>
2019-07-01 01:03:17 +0300

    Update HumanModel.java

--- 5_mvc/src/main/java/spring_mvc/model/HumanModel.java
+++ 5_mvc/src/main/java/spring_mvc/model/HumanModel.java
@@ -4,28 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-
+@Getter
+@Setter
+@ToString
 public class HumanModel {
 
     private String name;
     private String age;
 
     public HumanModel() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public void setAge(String age) {
-        this.age = age;
     }
 }
